4821ab2b01bb11b3629c2da439a00410950098c3
giscard <ntchinda1998@gmail.com>
2022-10-13 03:13:51 +0100

    changes on the component folder

---
+++ src/components/mainto.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Mainto extends Component {
+    state = {  } 
+    render() { 
+        return (<h>Hello World</h>);
+    }
+}
+ 
+export default Mainto;
