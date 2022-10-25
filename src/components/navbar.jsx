5b06ec5553a1c5873b3468a2ab0a8f67f4dc1b1e
giscard <ntchinda1998@gmail.com>
2022-10-25 19:49:25 +0100

    addes link and navlink

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
     state = {  } 
     render() { 
         return (
-            <nav className = "navbar navbar-expand-lg navbar-light bg-light">
-  <a className = "navbar-brand" href="#">Navbar</a>
+<nav className = "navbar navbar-expand-lg navbar-light bg-light">
+  <Link className = "navbar-brand" to="/"> <b>PSMaint</b> </Link>
   <button className = "navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span className = "navbar-toggler-icon"></span>
   </button>
-  <div className = "collapse navbar-collapse" id="navbarNav">
-    <ul className = "navbar-nav">
-      <li className = "nav-item active">
-        <a className = "nav-link" href="#"> Systhmatique <span className = "sr-only">(current)</span></a>
-      </li>
-      <li className = "nav-item">
-        <a className = "nav-link" href="#"> Conditionelle </a>
-      </li>
-      <li className = "nav-item">
-        <a className = "nav-link" href="#"> Formulaire </a>
-      </li>
-    </ul>
+  <div className = "collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div className='navbar-nav'>
+      <NavLink className = "nav-item nav-link" to="/maint"> Appareil <span className = "sr-only danger"></span></NavLink>
+      <NavLink className = "nav-item nav-link" to="/forms"> Formulaire </NavLink>
+    </div>
   </div>
 </nav>
         );
