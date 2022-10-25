4d56d95ece0fb98356c2c21b64079d56f3698217
giscard <ntchinda1998@gmail.com>
2022-10-25 16:02:26 +0100

    added forms

---
+++ src/components/formulaire.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Forms extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <h1>Forms</h1>
+        );
+    }
+}
+ 
+export default Forms;
