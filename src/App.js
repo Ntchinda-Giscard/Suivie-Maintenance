034c87bb4dc757c241939f0ac71eb6488daeeaf5
giscard <ntchinda1998@gmail.com>
2022-10-25 19:43:40 +0100

    added routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Mainto from './components/mainto';
+import Forms from './components/formulaire';
 import './App.css'
+import Test from './components/test';
 
 class App extends Component {
   state = {  } 
@@ -9,8 +12,13 @@ class App extends Component {
     return (
       <React.Fragment>
         <Navbar />
+        {/* <Forms /> */}
         <main className="container">
-          <Mainto />
+          <Switch>
+            <Route path='/maint' component={Mainto}></Route>
+            <Route path='/forms' compenent={Forms}></Route>
+            <Redirect from='/' exact to={Mainto} />
+          </Switch>      
        </main>
       </React.Fragment>    
     );
